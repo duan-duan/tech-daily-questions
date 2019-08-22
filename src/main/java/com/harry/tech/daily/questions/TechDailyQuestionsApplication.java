fdcdb686723283862d3f15ce3ff248944fdc941f
wangruirui <wangruirui@le.com>
2019-08-22 14:15:42 +0800

    first commit

---
+++ src/main/java/com/harry/tech/daily/questions/TechDailyQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.harry.tech.daily.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDailyQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechDailyQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/harry/tech/daily/questions/string/StringDemo.java
@@ -0,0 +1,9 @@
+package com.harry.tech.daily.questions.string;
+
+/**
+ * @Description: This is Description
+ * @author: wangruirui
+ * @date: 2019/08/22
+ */
+public class StringDemo {
+}
